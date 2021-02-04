01e4239b01b5905dc01411adaec4cdd2441c2378
MaxiDiaz <maxi.diaz66@gmail.com>
2021-02-04 10:25:02 -0300

    se agrego la validacion del email y el numero de telefono

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-campoRequerido = elemento => (elemento.value === '') ? elemento.className = 'form-control is-invalid' : elemento.className = 'form-control is-valid';
-
-validarEmail = (elemento) => {
-    (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test(elemento.value) && elemento.value !== '' ) ?  elemento.className = 'form-control is-valid' : elemento.className = 'form-control is-invalid';
+campoRequerido = elemento => {
+    if (elemento.value === '') {
+        elemento.className = 'form-control is-invalid';
+        return false;
+    } else {
+        elemento.className = 'form-control is-valid';
+        return true;
+    }
 }
+
+validarEmail = (elemento) => ((/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i).test(elemento.value) && campoRequerido(elemento)) ? elemento.className = 'form-control is-valid' : elemento.className = 'form-control is-invalid';
+
+validarTelefono = (elemento) => (campoRequerido(elemento) && !isNaN(elemento.value)) ? elemento.className = 'form-control is-valid' : elemento.className = 'form-control is-invalid';
