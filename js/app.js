13905f0a29f4b6c8762650df4044484b85b6b5ff
MaxiDiaz <maxi.diaz66@gmail.com>
2021-02-09 10:18:17 -0300

    Se agrego la funcion de validacion general del formulario con un alert correspondiente a la consulta. Falta validad el checkbox

--- js/app.js
+++ js/app.js
@@ -8,12 +8,64 @@ campoRequerido = elemento => {
     }
 }
 
-validarEmail = (elemento) => ((/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i).test(elemento.value) && campoRequerido(elemento)) ? elemento.className = 'form-control is-valid' : elemento.className = 'form-control is-invalid';
+validarEmail = (elemento) => {
+    if ((/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i).test(elemento.value) && campoRequerido(elemento)) {
+        elemento.className = 'form-control is-valid';
+        return true;
+    } else {
+        elemento.className = 'form-control is-invalid';
+        return false;
+    }
+}
 
-validarTelefono = (elemento) => (campoRequerido(elemento) && !isNaN(elemento.value)) ? elemento.className = 'form-control is-valid' : elemento.className = 'form-control is-invalid';
+validarTelefono = (elemento) => {
+    if (campoRequerido(elemento) && !isNaN(elemento.value)) {
+        elemento.className = 'form-control is-valid';
+        return true;
+    }
+    else {
+        elemento.className = 'form-control is-invalid';
+        return false;
+    }
+}
 
-cantidadCaracteres = (elemento) => (campoRequerido(elemento) && elemento.value.length > 10) ? elemento.className = 'form-control is-valid' : elemento.className = 'form-control is-invalid';
+cantidadCaracteres = (elemento) => {
+    if (campoRequerido(elemento) && elemento.value.length > 10) {
+        elemento.className = 'form-control is-valid';
+        return true;
+    }
+    else {
+        elemento.className = 'form-control is-invalid';
+        return false;
+    }
+}
 
-//agregar eventos de JS
-let checkbox = document.getElementById('checkTermino');
-checkbox.addEventListener('change', () => (checkbox.checked) ? checkbox.className = 'form-check-input is-valid' : checkbox.className = 'form-check-input is-invalid');
+// let checkbox = document.getElementById('checkTermino');
+// checkbox.addEventListener('change', validarCheckbox);
+
+// validarCheckbox = () => {
+//     if (checkbox.checked) {
+//         checkbox.className = 'form-check-input is-valid';
+//         return true;
+//     }
+//     else {
+//         checkbox.className = 'form-check-input is-invalid';
+//         return false;
+//     }
+// }
+
+validarGeneral = (event) => {
+    event.preventDefault();
+    let alerta = document.getElementById("mensajeEnvio");
+    if (campoRequerido(document.getElementById('nombre')) &&
+        validarEmail(document.getElementById('email')) &&
+        validarTelefono(document.getElementById('telefono')) &&
+        cantidadCaracteres(document.getElementById('consulta'))
+    ) {
+        alerta.className = "alert alert-success mx-3";
+        alerta.innerHTML = "Su consulta fue enviada correctamente.";
+    } else {
+        alerta.className = "alert alert-danger mx-3";
+        alerta.innerHTML = "Ocurrio un error. Verifique los datos ingresados.";
+    }
+}
